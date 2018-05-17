b6b1e34d229427440cefddfba8ebde141c528f8f
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2018-05-17 22:04:11 +0200

    Added an interactivity in the player

--- NNRE/main.go
+++ NNRE/main.go
@@ -2,25 +2,26 @@ package main
 
 import (
 	"context"
-	"io/ioutil"
 	"log"
 
 	"github.com/owulveryck/lstm"
 )
 
 func main() {
-	// Read the weights
-	b, err := ioutil.ReadFile("tictactoe.bin")
-	if err != nil {
-		log.Fatal("Cannot read binary file", err)
-	}
-	model := new(lstm.Model)
-	err = model.UnmarshalBinary(b)
-	if err != nil {
-		log.Fatal("Cannot restore software 2.0", err)
-	}
-	p := new(Player)
-	err = model.Predict(context.TODO(), p)
+	/*
+		b, err := ioutil.ReadFile("tictactoe.bin")
+		if err != nil {
+			log.Fatal("Cannot read binary file", err)
+		}
+		model := new(lstm.Model)
+		err = model.UnmarshalBinary(b)
+		if err != nil {
+			log.Fatal("Cannot restore software 2.0", err)
+		}
+	*/
+	model := lstm.NewModel(9, 9, 100)
+	p := NewPlayer()
+	err := model.Predict(context.TODO(), p)
 	if err != nil {
 		log.Println(err)
 	}
--- NNRE/player.go
+++ NNRE/player.go
@@ -1,12 +1,48 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
+
 // Player ...
-type Player struct{}
+type Player struct {
+	c chan int
+}
+
+// NewPlayer ...
+func NewPlayer() *Player {
+	c := make(chan int, 0)
+	go func() {
+		for {
+			fmt.Print("Enter move: ")
+			var input string
+			fmt.Scanln(&input)
+			move, err := strconv.Atoi(input)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			if move > 8 {
+				continue
+			}
+			c <- move
+		}
+	}()
+	return &Player{
+		c: c,
+	}
+}
 
 func (p *Player) Read() ([]float32, error) {
-	return nil, nil
+	move := <-p.c
+	output := make([]float32, 9)
+	output[move] = 1
+	return output, nil
 }
 
 func (p *Player) Write(v []float32) error {
+	log.Println(v)
 	return nil
 }
