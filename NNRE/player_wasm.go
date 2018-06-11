7a7870ce7a5c9fae42fb9642cefdff0b0d04a71d
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2018-06-11 14:04:56 +0200

    Compatibility with the html

--- NNRE/player_wasm.go
+++ NNRE/player_wasm.go
@@ -7,6 +7,38 @@ import (
 	"syscall/js"
 )
 
+type mycb struct {
+	id   string
+	cell int
+	p    *Player
+}
+
+func (m mycb) cb([]js.Value) {
+	move := m.cell
+	if move == 10 {
+		m.p.inputMove <- move
+	}
+	if move > 9 {
+		return
+	}
+	if move != 9 {
+		if m.p.board[move] == 1 || m.p.board[move+9] == 1 {
+			return
+		}
+		m.p.board[move] = 1
+	}
+
+	js.Global.Get("document").Call("getElementById", m.id).Set("innerHTML", js.ValueOf("O"))
+	m.p.visualBoard[move] = "O"
+	m.p.inputMove <- move
+	m.p.hasPlayed <- "O"
+	myMove := <-m.p.predictedMove
+	m.p.inputMove <- myMove
+	js.Global.Get("document").Call("getElementById", fmt.Sprintf("ttt%v", myMove)).Set("innerHTML", js.ValueOf("X"))
+	m.p.hasPlayed <- "X"
+
+}
+
 // NewPlayer ...
 func NewPlayer() *Player {
 	fmt.Println("New player")
@@ -18,12 +50,22 @@ func NewPlayer() *Player {
 		board:         make([]int, 18),
 		hasPlayed:     make(chan string, 0),
 	}
+	// Reset the content and set the handler
+	for i, v := range []string{"ttt0", "ttt1", "ttt2", "ttt3", "ttt4", "ttt5", "ttt6", "ttt7", "ttt8"} {
+		m := mycb{
+			v,
+			i,
+			p,
+		}
+		js.Global.Get("document").Call("getElementById", v).Call("addEventListener", "click", js.NewCallback(m.cb))
 
+	}
 	go func() {
 		inputMove <- 9
 		p.hasPlayed <- "O"
 		myMove := <-predictedMove
 		js.Global.Set("output", js.ValueOf(myMove))
+		js.Global.Get("document").Call("getElementById", fmt.Sprintf("ttt%v", myMove)).Set("innerHTML", js.ValueOf("X"))
 		inputMove <- myMove
 		p.hasPlayed <- "X"
 	}()
--- NNRE/wasm/server.go
+++ NNRE/wasm/server.go
@@ -1,23 +1,21 @@
 package main
 
 import (
+	"log"
+	"mime"
 	"net/http"
 
 	"github.com/urfave/negroni"
 )
 
-func wasmHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/wasm")
-	http.ServeFile(w, r, "test.wasm")
-}
 func main() {
+	mime.AddExtensionType(".wasm", "application/wasm")
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir(".")))
-	mux.HandleFunc("/test.wasm", wasmHandler)
 
 	n := negroni.Classic() // Includes some default middlewares
 	n.UseHandler(mux)
 
-	http.ListenAndServe(":3000", n)
+	log.Fatal(http.ListenAndServe(":3000", n))
 
 }
